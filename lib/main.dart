e4b5149e6fc512c47d4f040cea10ecca7c209588
Thananon <thananon@digio.co.th>
2020-11-16 21:21:43 +0700

    Learning how to use Row to manage layout

--- lib/main.dart
+++ lib/main.dart
@@ -12,24 +12,25 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.tealAccent,
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Container(
                 height: 100.0,
-                width: 100.0,
                 color: Colors.white,
                 child: Text('Container 1'),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 20.0,
+              ),
               Container(
                 height: 100.0,
-                width: 100.0,
                 color: Colors.blue,
                 child: Text('Container 2'),
               ),
               Container(
                 height: 100.0,
-                width: 100.0,
                 color: Colors.redAccent,
                 child: Text('Container 2'),
               ),
