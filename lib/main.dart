582b94ff50d978893d2baaa704216e79808086ad
Thananon <thananon@digio.co.th>
2020-12-02 11:15:54 +0700

    Learn how to use widget Cards, ListTile, Divider to make up the app look more gorgeous

--- lib/main.dart
+++ lib/main.dart
@@ -15,34 +15,84 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child:Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/diamond_profile_pic.png'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/diamond_profile_pic.png'),
+            ),
+            Text(
+              'Sterk',
+              style: TextStyle(
+                  fontSize: 40.0, color: Colors.white, fontFamily: 'Pacifico'),
+            ),
+            Text(
+              'Product Owner',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                  fontFamily: 'SourceSansPro'),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal[50],
               ),
-              Text(
-                'Sterk',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontFamily: 'Pacifico'
-                ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child:ListTile(
+                  leading:
+                  Icon(
+                    Icons.phone,
+                    size: 40.0,
+                    color: Colors.teal[900],
+                  ),
+                  title:
+                  Text(
+                    '+668 3151 8888',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0),
+                  ) ,
+                )
               ),
-              Text(
-                'Product Owner',
-                style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.teal[100],
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.5,
-                    fontFamily: 'SourceSansPro'
-                ),
-              )
-            ],
-          )
-        ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 40.0,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    'sthananon@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
